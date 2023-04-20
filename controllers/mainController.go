26d4e60a0ab1a78fce8d93f22f0416c01032bc11
melvin <mmpriyemskiy@gmail.com>
2023-04-20 16:31:41 -0400

    completed login middlewear implementation

--- controllers/mainController.go
+++ controllers/mainController.go
@@ -15,17 +15,20 @@ func (m *MainController) GetIndex(ctx *gin.Context) {
 	success := sessions.Default(ctx).Flashes("success")
 	errMsgs := sessions.Default(ctx).Flashes("error")
 
+	warningMes := sessions.Default(ctx).Flashes("warning")
+
 	// clearing the flash before rendering
 	sessions.Default(ctx).Flashes()
 	sessions.Default(ctx).Save()
 
 	ctx.HTML(http.StatusOK, "main.html", gin.H{
-		"Title":       "Hello there",
-		"Body":        "Welcome to the UNC Charlotte Blog Website.",
-		"Sample":      "Students can ask their peers for any help or share any advice for their peers relating to matters such as classes, clubs, sports, or other extracurricular activities.",
-		"successMsgs": success,
-		"errorMsgs":   errMsgs,
-		"User":        val,
+		"Title":            "Hello there",
+		"Body":             "Welcome to the UNC Charlotte Blog Website.",
+		"Sample":           "Students can ask their peers for any help or share any advice for their peers relating to matters such as classes, clubs, sports, or other extracurricular activities.",
+		"successMsgs":      success,
+		"errorMsgs":        errMsgs,
+		"User":             val,
+		"MyWarningMessage": warningMes,
 	})
 }
 
